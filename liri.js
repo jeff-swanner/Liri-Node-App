809ec73d76eaa46461bd9a5915b6b92a3a7dff5b
Jeff Swanner <jeffswanner93@gmail.com>
2019-09-07 09:21:18 -0400

    Added movie and concert search

--- liri.js
+++ liri.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
 var fs = require('fs');
+var axios = require("axios");
+var moment = require('moment');
 
 var spotify = new Spotify(keys.spotify);
 var userCommand = process.argv[2];
@@ -52,10 +54,43 @@ function spotifySearch(song) {
         console.log("Preview: " + songResult.preview_url);
         
     });
-}
+};
 
+function concertSearch(artist) {
+    var concertURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    axios.get(concertURL).then(
+        function(response) {
+            console.log(artist + " upcoming concerts:");
+            var k;
+            if (response.data[0].length<5) {
+                k = response.data[0].length;
+            } else {
+                k = 5;
+            };
+            for (i=0;i<k;i++) {
+                console.log("Venue: " + response.data[i].venue.name);
+                console.log("Location: " + response.data[i].venue.city + ", " + response.data[0].venue.country);
+                console.log("Date: " + moment(response.data[i].datetime).format('MM/DD/YYYY'));
+            };
+        }).catch(function(error) {
+            return console.log(error);
+        });
+};
 
-
-
+function movieSearch(movie) {
+    var movieURL = 'http://www.omdbapi.com/?apikey=trilogy&t=' + movie;
+    axios.get(movieURL).then(function(response) {
+        console.log("Movie: " + response.data.Title);
+        console.log("Release: " + response.data.Year);
+        console.log("IMDB Rating: " + response.data.imdbRating);
+        console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+        console.log("Country: " + response.data.Country);
+        console.log("Language: " + response.data.Language);
+        console.log("Plot: " + response.data.Plot);
+        console.log("Actors: " + response.data.Actors);
+    }).catch(function(error) {
+        console.log(error);
+    });
+};
 
 searchType(userCommand);
