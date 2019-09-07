de3c7b917c796b050e3d4acc900540bc157eeb0d
Jeff Swanner <jeffswanner93@gmail.com>
2019-09-07 13:46:56 -0400

    Added comments and Readme file

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # liri-node-app
+
+## Description
+
+This application is a command line application created using Node.js to search for upcoming concerts, songs, or movies. The app accepts four possilbe inputs from the command line followed by the specific band, movie, or song the user wants to search for. Based on the user input command a function is called that queries the specific API and logs the formatted response to the console.
+
+## Input Commands
+* concert-this
+    * Searches for the next 5 upcoming concerts for the selected artist
+* spotify-this-song 
+    * Searches the spotify API for song input
+* movie-this 
+    * Searches OMDB for movie input
+* do-what-it-says
+    * Grabs info from random.txt file and searches based on the data 
+
+## Example Command Line Input
+* node liri concert-this Flume
+    * Searches for upcoming Flume concerts
+
+## How To Use
+1. Download the github repository to your computer. 
+2. You will need to save your Spotify id and secret in a .env file saved in the same folder. Navigate to https://www.npmjs.com/package/node-spotify-api for more info on obtaining these. 
+    * In the .env file set SPOTIFY_ID equal to your id and SPOTIFY_SECRET equal to your secret
+3. Run npm install in the command line while in the project folder to install the neccesary node modules
+4. Finally, you can run the input commands in the terminal as outlined above
+
+## Technologies Used
+* Node.js - Used for core application
+* Dotenv - For accessing Spotify keys saved in .env file
+* fs - For reading input from text file
+* Axios - Used for querying API Urls
+* Moment - Used to format time for concert search
+* Spotify API - Used for searching song data
+* Bands In Town API - Used for searching upcoming concerts
+* OMDB API - Used for searching movie data
+
+## Creator
+Jeff Swanner
--- liri.js
+++ liri.js
@@ -1,14 +1,21 @@
-require("dotenv").config();
+// Used to access spotify keys in .env file
+require("dotenv").config(); 
+
+// Requires for all the node modules used in this project
 var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
 var fs = require('fs');
 var axios = require("axios");
 var moment = require('moment');
 
+// Loads the spotify keys into spotify variable
 var spotify = new Spotify(keys.spotify);
+
+// Assigns user input to variables
 var userCommand = process.argv[2];
 var searchItem = process.argv.splice(3).join(" ");
 
+// Calls correct search function based on user input
 function searchType(userInput) {
     if (userInput === "concert-this") {
         concertSearch(searchItem);
@@ -23,44 +30,66 @@ function searchType(userInput) {
     }
 };
 
+// Reads text file and recalls the search function with input
 function textSearch() {
     fs.readFile('./random.txt', "utf-8", function read(err, data) {
+        // Logs any read errors
         if (err) {
             return console.log(err);
-        }
+        };
         var arr = data.split(",");
         searchItem = arr[1];
         searchType(arr[0]);
     });
 };
 
+// Searches Spotify for song
 function spotifySearch(song) {
+    // Defaults song to The Sign by Ace of Base
+    if (song === "") {
+        song = "The Sign Ace of Base"
+    };
+
+    // Calls spotify search function
     spotify.search({ type: 'track', query: song }, function(err, data) {
+        // Logs any search errors
         if (err) {
           return console.log('Error occurred: ' + err);
-        }
+        };
+
+        // Displays only the first song result
         var songResult = data.tracks.items[0];
 
+        // Displays track name
         console.log("Track: " + songResult.name);
 
+        // Dislays artists 
         var artist = [];
         for (i=0;i<songResult.artists.length;i++) {
             artist.push(songResult.artists[i].name);
         };
         console.log("Artist(s): " + artist.join(", ")); 
 
+        // Displays album
         console.log("Album: " + songResult.album.name);
 
+        // Displays song preview URL
         console.log("Preview: " + songResult.preview_url);
-        
     });
 };
 
+// Searches bands in town for concerts and displays
 function concertSearch(artist) {
+    // Constructs search URL
     var concertURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+    // Axios call to URL
     axios.get(concertURL).then(
         function(response) {
+            // Logs heading for data
             console.log(artist + " upcoming concerts:");
+
+            // Logs 5 most recent concerts unless there are less than 5 upcoming
             var k;
             if (response.data[0].length<5) {
                 k = response.data[0].length;
@@ -68,18 +97,29 @@ function concertSearch(artist) {
                 k = 5;
             };
             for (i=0;i<k;i++) {
+                // Logs venue name
                 console.log("Venue: " + response.data[i].venue.name);
+
+                // Logs venue location
                 console.log("Location: " + response.data[i].venue.city + ", " + response.data[0].venue.country);
+
+                // Logs concert date using moment
                 console.log("Date: " + moment(response.data[i].datetime).format('MM/DD/YYYY'));
             };
         }).catch(function(error) {
-            return console.log(error);
+            // Displays any errors
+            return console.log(error.message);
         });
 };
 
+// Searhes OMDB for specific movie
 function movieSearch(movie) {
+    // Create movie URL
     var movieURL = 'http://www.omdbapi.com/?apikey=trilogy&t=' + movie;
+
+    // Axios call to movieURL
     axios.get(movieURL).then(function(response) {
+        // Displays data to console
         console.log("Movie: " + response.data.Title);
         console.log("Release: " + response.data.Year);
         console.log("IMDB Rating: " + response.data.imdbRating);
@@ -89,8 +129,10 @@ function movieSearch(movie) {
         console.log("Plot: " + response.data.Plot);
         console.log("Actors: " + response.data.Actors);
     }).catch(function(error) {
+        // Displays any errors
         console.log(error);
     });
 };
 
+// Initial call for searchType function
 searchType(userCommand);
